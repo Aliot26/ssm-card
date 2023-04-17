2cbb021cce43652c0aca2b766ddad9d330961501
Aliot26 <olgakochanowa@gmail.com>
2023-04-17 15:00:31 +0200

    first commit

---
+++ src/main/java/com/volha/ssmcard/SsmCardApplication.java
@@ -0,0 +1,13 @@
+package com.volha.ssmcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volha/ssmcard/SsmCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volha.ssmcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
