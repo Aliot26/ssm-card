de04030db4165b7c6954c814b5673d733c46ea11
Aliot26 <olgakochanowa@gmail.com>
2023-04-18 15:58:25 +0200

    added states configuration

--- src/main/java/com/volha/ssmcard/config/StateMachineConfig.java
+++ src/main/java/com/volha/ssmcard/config/StateMachineConfig.java
@@ -1,4 +1,26 @@
 package com.volha.ssmcard.config;
 
-public class StateMachineConfig {
+import com.volha.ssmcard.domain.PaymentEvent;
+import com.volha.ssmcard.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+@Slf4j
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
 }
