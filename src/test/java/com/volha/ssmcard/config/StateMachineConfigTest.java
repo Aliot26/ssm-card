dd2c1ccda023939386d5ee049ca207a11998e621
Aliot26 <olgakochanowa@gmail.com>
2023-04-21 14:38:11 +0200

    added state configuration

---
+++ src/test/java/com/volha/ssmcard/config/StateMachineConfigTest.java
@@ -0,0 +1,38 @@
+package com.volha.ssmcard.config;
+
+import com.volha.ssmcard.domain.PaymentEvent;
+import com.volha.ssmcard.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+
+        sm.start();
+        System.out.println("StateMachine is started");
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+        System.out.println(sm.getState().toString());
+    }
+}
